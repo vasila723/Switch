794212076da185c3342459ee91c4615b1a24d96a
vasila723 <vasilamadusmonova723@gmail.com>
2023-05-29 11:36:57 +0300

    switch backgruond

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+}
